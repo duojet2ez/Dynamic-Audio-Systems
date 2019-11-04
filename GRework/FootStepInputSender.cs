881fcdc60613a1b111ae424380d95a985e3fb2fa
Gonzako <al341522@uji.es>
2019-11-04 10:21:17 +0100

    added simple way to add own custom input events

---
+++ GRework/FootStepInputSender.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepInputSender : MonoBehaviour
+{
+
+    //GB savers
+    float inputAngle;
+    int octant;
+    DirectionStep tempDir;
+
+
+    public event Action<DirectionStep, float> footStep;
+
+    private List<Action<Vector2>> registeredAction = new List<Action<Vector2>>();
+
+    public void ListenFor(ref Action<Vector2> inputEvent)
+    {
+        inputEvent += invokeFootStep;
+        registeredAction.Add(inputEvent);
+    }
+    public void StopListenFor(ref Action<Vector2> inputEvent)
+    {
+        inputEvent -= invokeFootStep;
+        registeredAction.Remove(inputEvent);
+    }
+
+    private void invokeFootStep(Vector2 input)
+    {
+        footStep?.Invoke(inputToDirection(input), input.magnitude);
+    }
+
+    private DirectionStep inputToDirection(Vector2 input)
+    {
+        inputAngle = (float)Math.Atan2(input.x, input.y);
+        octant = (int)Math.Round(8 * inputAngle/(2*Math.PI) + 8) % 8;
+        tempDir = (DirectionStep)(Mathf.Pow(2f, octant));
+        return tempDir;
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < registeredAction.Count; i++)
+        {
+            registeredAction[i] -= invokeFootStep;
+        }
+    }
+}
+
+[System.Flags]
+public enum DirectionStep //North == forward
+{
+    E = 1, NE = 2,
+    N = 4, NW = 8,
+    W = 16, SW = 32,
+    S = 64, SE = 128
+};
---
+++ GRework/MockPlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FootStepInputSender))]
+public class MockPlayerController : MonoBehaviour
+{
+    [SerializeField] float movementAmount = 2f;
+    public event Action<Vector2> inputEvent;
+    private FootStepInputSender InputSenderFoot;
+
+    //GB savers
+    Vector2 inputVector = new Vector2();
+
+    void GetRandomInput()
+    {
+        inputVector.x += UnityEngine.Random.Range(-0.3f, 0.3f);
+        inputVector.y += UnityEngine.Random.Range(-0.3f, 0.3f);
+        inputVector.x = Mathf.Clamp(inputVector.x, -1, 1);
+        inputVector.y = Mathf.Clamp(inputVector.y, -1, 1);
+
+        inputEvent.Invoke(inputVector);
+    }
+
+
+    void Start()
+    {
+        inputVector = new Vector2();
+        InputSenderFoot = GetComponent<FootStepInputSender>();
+        InputSenderFoot.ListenFor(ref inputEvent);
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetRandomInput();
+        transform.position += (Vector3)inputVector * (Time.fixedDeltaTime * movementAmount);
+    }
+}
